1a022064078a1d071f0b0c2b5f1125f39cc804c7
Christopher Arias <chrstphr.aaron@gmail.com>
2020-07-21 00:36:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function  happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown(holiday, )
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
